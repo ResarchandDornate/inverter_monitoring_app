a59ce07cf483c66a524e2b1a1fa5920b26652fb0
Ubuntu <ubuntu@ip-172-31-41-29.ap-south-1.compute.internal>
2026-01-23 12:19:37 +0000

    Production settings for EC2

--- inverter_app_backend/settings.py
+++ inverter_app_backend/settings.py
@@ -112,7 +112,7 @@ CORS_ALLOW_HEADERS = [
 
 # Security settings for production
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT =  False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_HSTS_SECONDS = 31536000  # 1 year
